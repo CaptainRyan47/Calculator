6b266a8df329f4b931e63c807d6be1f3920038bf
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-12 13:35:08 -0700

    Create operate function

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ function subtract(a, b) { return a - b };
 function multiply(a, b) { return a * b };
 
 function divide(a, b) { return a / b };
+
+//-----------------------------------
+
+function operate(firstInput, operator, secondInput) {
+  switch (operator) {
+    case 'add':
+      return add(firstInput, secondInput);
+      break;
+
+    case 'subtract':
+      return subtract(firstInput, secondInput);
+      break;
+
+    case 'multiply':
+      return multiply(firstInput, secondInput);
+      break;
+
+    case 'divide':
+      return divide(firstInput, secondInput);
+      break;
+
+    default:
+      break;
+  }
+}
+
+let firstInput;
+let operator;
+let secondInput;
+
