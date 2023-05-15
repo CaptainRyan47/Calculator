a24d6111b682e13215bae2232db92edd6643a7fa
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-15 15:46:20 -0700

    Add basic functionality so calculator is now usable
    There are some things still not working as expected but it can now do most things properly.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Calculation Functions
 
-function add(a, b) { return a + b };
+function add(a, b) { return Number(a) + Number(b) };
 
 function subtract(a, b) { return a - b };
 
@@ -10,7 +10,80 @@ function divide(a, b) { return a / b };
 
 //-----------------------------------
 
-function operate(firstInput, operator, secondInput) {
+let firstInput = '';
+let operator = null;
+let secondInput = '';
+let readyForSecondInput = false;
+
+function resetInputs(bool) {
+  firstInput = '';
+  operator = null;
+  secondInput = '';
+  readyForSecondInput = false;
+  if (bool) display.textContent = '0';
+}
+
+//-----------------------------------
+
+const display = document.querySelector('#display');
+
+//Number Buttons:
+document.querySelectorAll('#buttons .number').forEach(
+  (button) => button.addEventListener('click', (button) => inputNum(button.target.id.charAt(1)))
+);
+
+//Other Buttons:
+document.querySelector('#add').addEventListener('click', (button) => inputOperator(button.target.id));
+document.querySelector('#subtract').addEventListener('click', (button) => inputOperator(button.target.id));
+document.querySelector('#multiply').addEventListener('click', (button) => inputOperator(button.target.id));
+document.querySelector('#divide').addEventListener('click', (button) => inputOperator(button.target.id));
+document.querySelector('#equals').addEventListener('click', () => {
+  display.textContent = operate();
+  resetInputs();
+});
+document.querySelector('#clear').addEventListener('click', () => resetInputs(true));
+document.querySelector('#delete').addEventListener('click', () => {
+  if (readyForSecondInput) {
+    secondInput = secondInput.substring(0, secondInput.length - 1);
+    display.textContent = secondInput;
+  }
+  else {
+    firstInput = firstInput.substring(0, firstInput.length - 1);
+    display.textContent = firstInput;
+  }
+});
+
+//-----------------------------------
+
+function inputNum(button) {
+  if (!readyForSecondInput) {
+    firstInput += button;
+    console.log(firstInput);
+    display.textContent = firstInput;
+  } else {
+    secondInput += button;
+    console.log(secondInput);
+    display.textContent = secondInput;
+  }
+}
+
+function inputOperator(button) {
+  if (firstInput === '' && !readyForSecondInput) return;
+  else if (!operator) {
+    operator = button;
+    console.log(operator);
+    readyForSecondInput = true;
+  } else {
+    firstInput = operate();
+    display.textContent = firstInput;
+    secondInput = '';
+    operator = button;
+  }
+};
+
+
+
+function operate() {
   switch (operator) {
     case 'add':
       return add(firstInput, secondInput);
@@ -29,11 +102,7 @@ function operate(firstInput, operator, secondInput) {
       break;
 
     default:
+      return 0;
       break;
   }
 }
-
-let firstInput;
-let operator;
-let secondInput;
-
