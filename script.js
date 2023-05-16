82d758662bad4633009581601659c3718e9812a2
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-16 11:36:48 -0700

    Add functionality for decimal input and rounding answers

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //Calculation Functions
 
-function add(a, b) { return Number(a) + Number(b) };
+function add(a, b) { return (Math.floor((Number(a) + Number(b)) * 1000)) / 1000 };
 
-function subtract(a, b) { return a - b };
+function subtract(a, b) { return (Math.floor((a - b) * 1000)) / 1000 };
 
-function multiply(a, b) { return a * b };
+function multiply(a, b) { return (Math.floor((a * b) * 1000)) / 1000 };
 
-function divide(a, b) { return a / b };
+function divide(a, b) {
+  if (b === '0') console.log('boom!');
+  return (Math.floor((a / b) * 1000)) / 1000
+};
 
 //-----------------------------------
 
@@ -14,14 +17,16 @@ let firstInput = '';
 let operator = null;
 let secondInput = '';
 let readyForSecondInput = false;
+let decimalToggle = false;
 
 function resetInputs(bool) {
   firstInput = '';
   operator = null;
   secondInput = '';
   readyForSecondInput = false;
+  decimalToggle = false;
   if (bool) display.textContent = '0';
-}
+};
 
 //-----------------------------------
 
@@ -31,6 +36,7 @@ const display = document.querySelector('#display');
 document.querySelectorAll('#buttons .number').forEach(
   (button) => button.addEventListener('click', (button) => inputNum(button.target.id.charAt(1)))
 );
+document.querySelector('#decimal').addEventListener('click', () => decimalHandler('.'));
 
 //Other Buttons:
 document.querySelector('#add').addEventListener('click', (button) => inputOperator(button.target.id));
@@ -44,10 +50,12 @@ document.querySelector('#equals').addEventListener('click', () => {
 document.querySelector('#clear').addEventListener('click', () => resetInputs(true));
 document.querySelector('#delete').addEventListener('click', () => {
   if (readyForSecondInput) {
+    if (secondInput.charAt(secondInput.length - 1) === '.') decimalToggle = false;
     secondInput = secondInput.substring(0, secondInput.length - 1);
     display.textContent = secondInput;
   }
   else {
+    if (firstInput.charAt(firstInput.length - 1) === '.') decimalToggle = false;
     firstInput = firstInput.substring(0, firstInput.length - 1);
     display.textContent = firstInput;
   }
@@ -65,6 +73,12 @@ function inputNum(button) {
     console.log(secondInput);
     display.textContent = secondInput;
   }
+};
+
+function decimalHandler(decimal) {
+  if (decimalToggle) return;
+  else inputNum(decimal);
+  decimalToggle = true;
 }
 
 function inputOperator(button) {
@@ -73,15 +87,17 @@ function inputOperator(button) {
     operator = button;
     console.log(operator);
     readyForSecondInput = true;
+    decimalToggle = false;
   } else {
     firstInput = operate();
-    display.textContent = firstInput;
     secondInput = '';
     operator = button;
   }
 };
 
-
+function mf(num) {
+  return (Math.floor(num * 1000)) / 1000;
+}
 
 function operate() {
   switch (operator) {
@@ -105,4 +121,4 @@ function operate() {
       return 0;
       break;
   }
-}
+};
